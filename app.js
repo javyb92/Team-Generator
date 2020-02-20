2bc6332737688205b93619a30f771800f924b538
jab_tx <javier.a.baragan92@gmail.com>
2020-02-20 08:05:06 -0600

    save point, working on prompts

--- app.js
+++ app.js
@@ -1,40 +1,48 @@
 const inquirer = require("inquirer");
-const fs = require ("fs")
+// const fs = require ("fs") //save for later, maybe
 
 // const Employee = require("./lib/Employee");
-// const employee = new Employee();
+
+
+//Initial questions at startup
 function employeeInfo(){
     return inquirer.prompt([
         {
             type: "input",
             message:"What is your Employees name?",
-            name:"employeeName"
+            name: "name"
          },
          {
             type: "input",
             message:"What is your Employees ID number?",
-            name:"employeeID"
+            name:"id"
          },
          {
             type: "input",
             message:"What is your Employees email address?",
-            name:"employeeEmail"
+            name:"email"
          },
          {
             type: "list",
             message:"What is your Employees role?",
-            name:"employeeName",
-            choices: ["Manager", "Intern","Engineer"]
+            name:"title",
+            choices: 
+            ["Manager", 
+            "Intern",
+            "Engineer"]
          },
     ]);
-}
+};
 
 
+
+//Starts the app
 async function init() {
     try{
-
         const answers = await employeeInfo();
-
+        const name = answers.name;
+        const id = answers.id;
+        const email = answers.email;
     }
     catch(err){
         console.log(err)

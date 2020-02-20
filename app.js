a99425d99a667bdf7a445d99883ec7ddfc0b7383
jab_tx <javier.a.baragan92@gmail.com>
2020-02-20 05:34:10 -0600

    added working prompt

--- app.js
+++ app.js
@@ -1,5 +1,46 @@
-const Employee = require("./lib/Employee");
+const inquirer = require("inquirer");
+const fs = require ("fs")
 
-const employee = new Employee();
+// const Employee = require("./lib/Employee");
+// const employee = new Employee();
+function employeeInfo(){
+    return inquirer.prompt([
+        {
+            type: "input",
+            message:"What is your Employees name?",
+            name:"employeeName"
+         },
+         {
+            type: "input",
+            message:"What is your Employees ID number?",
+            name:"employeeID"
+         },
+         {
+            type: "input",
+            message:"What is your Employees email address?",
+            name:"employeeEmail"
+         },
+         {
+            type: "list",
+            message:"What is your Employees role?",
+            name:"employeeName",
+            choices: ["Manager", "Intern","Engineer"]
+         },
+    ]);
+}
 
-employee.run();
+
+async function init() {
+    try{
+
+        const answers = await employeeInfo();
+
+    }
+    catch(err){
+        console.log(err)
+        inquirer.prompt(answers)
+    }
+}
+
+
+init();
